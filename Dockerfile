d56eb1ff917ca6d32bc09505afd1a192a6dfa9a8
Kare Koho <kare.koho@yahoo.com>
2019-04-02 22:36:36 +0300

    Add googletest library and sources

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,10 @@
 FROM ubuntu:latest
-ARG GTEST_DIR=/usr/local/src/gtest
-COPY ./gtest ${GTEST_DIR}
-WORKDIR /valgrind
+ARG GTEST_DIR=/usr/local/src/googletest
+COPY ./googletest ${GTEST_DIR}
+# WORKDIR /valgrind
 RUN apt-get update
 RUN apt-get upgrade -y
-# RUN apt-get install build-essential qt5-default valgrind -y
 RUN apt-get install build-essential valgrind -y
-RUN g++ -std=c++11 -isystem ${GTEST_DIR}/include -I${GTEST_DIR} \
-    -pthread -c ${GTEST_DIR}/src/gtest-all.cc
-RUN ar -rv libgtest.a gtest-all.o
+# RUN apt-get qt5-default
+RUN cd ${GTEST_DIR}/make && make && make install
+WORKDIR /valgrind
---
+++ googletest/build/CMakeFiles/sample7_unittest.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/kare/devel/googletest/googletest/samples/sample7_unittest.cc" "/Users/kare/devel/googletest/googletest/build/CMakeFiles/sample7_unittest.dir/samples/sample7_unittest.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "../"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/Users/kare/devel/googletest/googletest/build/CMakeFiles/gtest_main.dir/DependInfo.cmake"
+  "/Users/kare/devel/googletest/googletest/build/CMakeFiles/gtest.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
